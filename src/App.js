f8b2203987e03b5c8e664940354c19f04adb1e53
bayfight <bayu161292@gmail.com>
2018-12-11 14:54:35 +0700

    baru

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      profil : "",
+      input : "",
+    }
+  }
   render() {
+    let klik = (e) => {
+      // let nama = document.getElementById('tol').value
+      this.setState({
+        profil: `https://robohash.org/${this.state.input}`
+      })
+    }
+
+    let berubah = (e) => {
+      this.setState({
+        input: e.target.value 
+      })
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="col-lg-12">
+        <div className="row">
+        <div className="container">
+        <div className="col-lg-6 ml">
+        <div id="cardRobot" class="card text-center">
+          <div class="card-body">
+            <input type="text" placeholder="Ketik nama Anda..." onChange={berubah} ></input>
+            <br></br>
+            <button type="button" class="btn btn-success mt-2" onClick={klik}>Buat Profil Picture</button>
+            <br></br>
+            <img className="rounded-circle mt-3 bg-white" src={this.state.profil}/>
+          </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
       </div>
     );
   }
